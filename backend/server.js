08c0a3b177ec7bcc1b12421cedb2c72ccf55a4cb
Pravin Jadhav <pr.jadhav02@gmail.com>
2022-01-08 21:40:00 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,24 @@
 const express = require('express');
-
+const notes = require('./data/notes');
+const dotenv = require('dotenv');
 const app = express();
-app.listen(5000,console.log('Listening on port'))
+dotenv.config();
+
+
+app.get("/", (req,res)=>{ 
+    res.send("API is running..");
+});
+
+
+app.get('/api/notes',(req,res)=>{
+res.json(notes)
+
+app.get('/api/notes/:id',(req,res)=>{
+    const note = notes.find((n)=>n._id === req.params.id);
+
+    res.send(note);
+});
+
+})
+
+app.listen(5000,console.log("server started on port 5000"));
